92ad44a01e38f0e675b187d97d3ba798e2f1eb42
mjharrison <mjsonofharry@gmail.com>
2019-01-13 15:49:57 -0500

    minimal working mod

---
+++ src/main/java/com/mjsonofharry/assembler/Assembler.java
@@ -0,0 +1,31 @@
+package com.mjsonofharry.assembler;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = Assembler.MODID, name = Assembler.NAME, version = Assembler.VERSION)
+public class Assembler
+{
+    public static final String MODID = "assembler";
+    public static final String NAME = "Assembler";
+    public static final String VERSION = "0.1.0";
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
