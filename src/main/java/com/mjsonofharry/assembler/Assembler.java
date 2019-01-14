9a8a8dc93a651e746e4f8ec8b5c5988d844388dd
mjharrison <mjsonofharry@gmail.com>
2019-01-13 20:08:23 -0500

    c-style curly brace formatting vertical space is cheap and readability is valuable

--- src/main/java/com/mjsonofharry/assembler/Assembler.java
+++ src/main/java/com/mjsonofharry/assembler/Assembler.java
@@ -2,17 +2,22 @@ package com.mjsonofharry.assembler;
 
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.renderer.texture.ITickable;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraft.world.World;
 import net.minecraft.item.ItemStack;
+import net.minecraft.creativetab.CreativeTabs;
 
 import java.awt.Container;
 
@@ -44,13 +49,23 @@ public class Assembler
 
 class AssemblerBlockContainer extends BlockContainer
 {
-    AssemblerBlockContainer() {
+    AssemblerBlockContainer()
+    {
         super(Material.ROCK);
     }
 
     @Override
-    public TileEntity createNewTileEntity(World worldIn, int meta) {
+    public TileEntity createNewTileEntity(World worldIn, int meta)
+    {
         return new AssemblerTileEntity();
+    }
+    
+    @Override
+    public boolean onBlockActivated(
+        World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn,
+        EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ)
+    {
+        return super.onBlockActivated(worldIn, pos, state, playerIn, hand, facing, hitX, hitY, hitZ);
     }
 }
 
@@ -62,92 +77,110 @@ class AssemblerContainer extends Container
 class AssemblerTileEntity extends TileEntity implements IInventory, ITickable
 {
     @Override
-    public void clear() {
+    public void clear()
+    {
         
     }
 
     @Override
-    public void closeInventory(EntityPlayer player) {
+    public void closeInventory(EntityPlayer player)
+    {
         
     }
 
     @Override
-    public ItemStack decrStackSize(int index, int count) {
+    public ItemStack decrStackSize(int index, int count)
+    {
         return null;
     }
 
     @Override
-    public int getField(int id) {
+    public int getField(int id)
+    {
         return 0;
     }
 
     @Override
-    public int getFieldCount() {
+    public int getFieldCount()
+    {
         return 0;
     }
 
     @Override
-    public int getInventoryStackLimit() {
+    public int getInventoryStackLimit()
+    {
         return 0;
     }
 
     @Override
-    public String getName() {
+    public String getName()
+    {
         return null;
     }
 
     @Override
-    public int getSizeInventory() {
+    public int getSizeInventory()
+    {
         return 0;
     }
 
     @Override
-    public ItemStack getStackInSlot(int index) {
+    public ItemStack getStackInSlot(int index)
+    {
         return null;
     }
 
     @Override
-    public boolean hasCustomName() {
+    public boolean hasCustomName()
+    {
         return false;
     }
 
     @Override
-    public boolean isEmpty() {
+    public boolean isEmpty()
+    {
         return false;
     }
 
     @Override
-    public boolean isItemValidForSlot(int index, ItemStack stack) {
+    public boolean isItemValidForSlot(int index, ItemStack stack)
+    {
         return false;
     }
 
     @Override
-    public boolean isUsableByPlayer(EntityPlayer player) {
+    public boolean isUsableByPlayer(EntityPlayer player)
+    {
         return false;
     }
 
     @Override
-    public void openInventory(EntityPlayer player) {
+    public void openInventory(EntityPlayer player)
+    {
         
     }
 
     @Override
-    public ItemStack removeStackFromSlot(int index) {
+    public ItemStack removeStackFromSlot(int index)
+    {
         return null;
     }
 
     @Override
-    public void setField(int id, int value) {
+    public void setField(int id, int value)
+    {
         
     }
 
     @Override
-    public void setInventorySlotContents(int index, ItemStack stack) {
+    public void setInventorySlotContents(int index, ItemStack stack)
+    {
         
     }
 
     @Override
-    public void tick() {
+    public void tick()
+    {
         
     }
 }
